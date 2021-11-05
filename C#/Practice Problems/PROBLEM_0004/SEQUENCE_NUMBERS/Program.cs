dee376979ee0caf66e0f256b5fab7f9d99913cf1
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-05 19:32:04 +0100

    Problem_0004

---
+++ C#/Practice Problems/PROBLEM_0004/SEQUENCE_NUMBERS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEQUENCE_NUMBERS
+
+/*
+ * Write a program that prints the first 100 members of the sequence 2, -3, 4, -5, 6, -7, 8.
+ */
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int contador;
+
+			for (contador = 2; contador < 102; contador++)
+			{
+				Console.WriteLine(Math.Abs(contador));
+				contador++;
+				Console.WriteLine((Math.Abs(contador) * (-1)));
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
