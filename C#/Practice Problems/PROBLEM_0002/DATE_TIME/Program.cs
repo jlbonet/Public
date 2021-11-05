a9a0c3a654015a4abb36da35c1c8be07450e37d0
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-05 19:12:42 +0100

    Problem_0002

---
+++ C#/Practice Problems/PROBLEM_0002/DATE_TIME/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATE_TIME
+
+/*
+ * Write a program that prints on the console the current date and time
+ */
+
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fechaHora = DateTime.Now;
+			
+			Console.WriteLine($"The current date is: {fechaHora.ToShortDateString()}");
+			Console.WriteLine($"The current time is: {fechaHora.ToShortTimeString()}");
+			Console.ReadLine();
+		}
+	}
+}
