10c522d7535d4ad314493d1d1ec04800a80c1c16
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-05 19:20:43 +0100

    Problem_003

---
+++ C#/Practice Problems/PROBLEM_0003/SQUARE_ROOT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQUARE_ROOT
+/*
+ * Write a program that prints the square root of 12345.
+ */
+
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float squareRoot = 12345f;
+
+			Console.WriteLine($"The square root of 12345 is: {Math.Sqrt(squareRoot)}");
+			Console.ReadLine();
+		}
+	}
+}
