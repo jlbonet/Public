4909ddab972564c5b349d28af4f15c2b29e9ff90
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-06 08:45:06 +0100

    Problem_0006

---
+++ C#/Practice Problems/PROBLEM_0006/Substract With Out Substratc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substract_With_Out_Substratc
+
+/*
+* Subtract two numbers without using the subtract property.
+*/
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numero1 = 5;
+			int numero2 = 1;
+
+			Console.WriteLine($"The substract without substract is: {numero1} + {numero2} = {Math.Abs(numero1) + (Math.Abs(numero2) * (-1))}") ;
+			Console.ReadLine();
+		}
+	}
+}
