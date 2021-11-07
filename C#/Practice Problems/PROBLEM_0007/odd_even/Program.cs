db121d827406968919cbb74878463255441828aa
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-07 21:01:53 +0100

    Update

---
+++ C#/Practice Problems/PROBLEM_0007/odd_even/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_even
+
+/*
+ * Write an expression that checks whether an integer is odd or even.
+ */
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random aleatorio = new Random();
+			
+			for (int contador = 0; contador <= 50; contador++)
+			{
+				ulong numero = ((ulong)aleatorio.Next(1500000));
+				if (numero % 2 == 0)
+				{
+					Console.WriteLine($"The number {numero} is odd");
+				}
+
+				else Console.WriteLine($"The number {numero} is even");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
