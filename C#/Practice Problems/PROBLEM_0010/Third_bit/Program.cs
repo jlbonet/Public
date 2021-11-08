60f3f09da369c8f620bd179735273eba1b77109b
José Luis Domínguez Bonet <sistemas@JLBONET.ES>
2021-11-08 09:42:42 +0100

    Update

--- C#/Practice Problems/PROBLEM_0010/Third_bit/Program.cs
+++ C#/Practice Problems/PROBLEM_0010/Third_bit/Program.cs
@@ -7,29 +7,33 @@ using System.Threading.Tasks;
 namespace Third_Digit
 
 /*
- * Write an expression that looks for a given integer if its third digit (right to left) is 7.
+ *Write an expression that checks whether the third bit in a given integer is 1 or 0
  */
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Random aleatorio = new Random(1, 10);
+			Random aleatorio = new Random();
 
 			for (int contador = 0; contador < 50; contador++)
 			{
-				ulong numero = (ulong)aleatorio.Next();
-				string cadena = numero.ToString();
-				char caracter;
-				caracter = cadena[4];
+				ulong numero = (ulong)aleatorio.Next(1500000);
+				numero[]
+				//string cadena = numero.ToString();
+				//byte numeroBit = ((byte)numero);
+				
+				//numeroBit.
+				//char caracter;
+				//caracter = cadena[2];
 				if (caracter == '7')
 				{
 					Console.WriteLine($"This number {numero} contains a 7 in third psotion");
 				}
 
-				else 
-				{ 
-					//Console.WriteLine($"This number {numero} NOT is valid"); 
+				else
+				{
+					Console.WriteLine($"This number {numero} NOT is valid");
 				}
 			}
 			Console.ReadLine();
--- C#/Practice Problems/TEST/ConsoleApp1/Program.cs
+++ C#/Practice Problems/TEST/ConsoleApp1/Program.cs
@@ -14,22 +14,22 @@ namespace Third_Digit
 	{
 		static void Main(string[] args)
 		{
-			Random aleatorio = new Random(1, 10);
+			Random aleatorio = new Random();
 
 			for (int contador = 0; contador < 50; contador++)
 			{
-				ulong numero = (ulong)aleatorio.Next();
+				ulong numero = (ulong)aleatorio.Next(1500000);
 				string cadena = numero.ToString();
 				char caracter;
-				caracter = cadena[4];
+				caracter = cadena[2];
 				if (caracter == '7')
 				{
 					Console.WriteLine($"This number {numero} contains a 7 in third psotion");
 				}
 
-				else 
-				{ 
-					//Console.WriteLine($"This number {numero} NOT is valid"); 
+				else
+				{
+					Console.WriteLine($"This number {numero} NOT is valid");
 				}
 			}
 			Console.ReadLine();
