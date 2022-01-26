ae9383b923aba5a4ca92bec5c00be80a7c92e0cd
jlbonet <sistemas@jlbonet.es>
2022-01-26 21:39:21 +0100

    Calculadora fracciones

---
+++ Python/calculadoras/fracciones.py
@@ -0,0 +1,34 @@
+"""
+Programa para la creación de una calculadora de fracciones mediante el uso de clases.
+
+Operaciones:
+Sumar
+Restar
+Multiplicar
+Dividir
+
+"""
+
+class Fracciones :
+    
+    numerador   = 0
+    denominador = 1
+    
+    def __init__(self, numerador, denominador) :
+        
+        self.numerador      = numerador
+        self.denominador    = denominador
+        
+    def sumar(self, suma_numerador, suma_denominador) :
+
+        suma_numerador      = self.numerador
+        suma_denominador    = self.denominador
+        
+        if 
+        
+        return print(suma_numerador, suma_denominador)
+
+a = Fracciones(7,4)
+
+print(a.sumar())
+        
